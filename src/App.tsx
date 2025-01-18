568f20a29415ae66ad0c5099dada6758f88de216
david-kai-landes <davidkailandes@gmail.com>
2025-01-18 17:09:04 -0500

    added more style but not happy with structure, i am going to refactor some of the code

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import CardList from "./components/CardList";
 import "../src/styles/App.css";
 
 function App() {
-  // const [count, setCount] = useState(0);
-
   return (
     <div>
       <CardList />
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,8 +1,10 @@
 import * as React from "react";
-import Box from "@mui/material/Box";
+// import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import Typography from "@mui/material/Typography";
 import styles from "../styles/card.module.css";
 import paperImage from "../imgs/paper.png";
@@ -28,7 +30,7 @@ const AssetCard: React.FC<CardsProps> = ({
 }) => {
   return (
     <>
-      <Box className={styles.badge}>{category}</Box>
+      <div className={styles.badge}>{category}</div>
       <div className={styles.cardWrapper}>
         <CardMedia
           component="img"
@@ -49,15 +51,81 @@ const AssetCard: React.FC<CardsProps> = ({
         >
           <CardContent className={styles.cardContent}>
             <div className={styles.dataSection}>
-              <div className={styles.dataColumn}>
-                <Typography>{price}</Typography>
-                <Typography>Price</Typography>
-                <Typography>{change} </Typography>
-                <Typography>Change 24hrs</Typography>
+              <div className={styles.dataColumnOne}>
+                {category === "Stock" ? (
+                  <>
+                    <Typography className={styles.price}>{price}</Typography>
+                    <Typography className={styles.priceText}>Price</Typography>
+                    <Typography className={styles.marketCap}>
+                      {marketCap}
+                    </Typography>
+                    <Typography className={styles.marketCapText}>
+                      Market Cap
+                    </Typography>
+                  </>
+                ) : category === "Crypto" ? (
+                  <>
+                    <Typography className={styles.price}>{price}</Typography>
+                    <Typography className={styles.priceText}>Price</Typography>
+                    <Typography className={styles.marketCap}>
+                      {marketCap}
+                    </Typography>
+                    <Typography className={styles.marketCapText}>
+                      Market Cap
+                    </Typography>
+                  </>
+                ) : (
+                  <>
+                    <Typography className={styles.price}>{price}</Typography>
+                    <Typography className={styles.priceText}>
+                      Floor Price
+                    </Typography>
+                    <Typography className={styles.marketCap}>
+                      {marketCap}
+                    </Typography>
+                    <Typography className={styles.marketCapText}>
+                      Total volume
+                    </Typography>
+                  </>
+                )}
               </div>
-              <div className={styles.dataColumn}>
-                <Typography>{marketCap}</Typography>
-                <Typography>Market Cap</Typography>
+
+              <div className={styles.dataColumnTwo}>
+                {category === "Stock" ? (
+                  <>
+                    <div className={styles.changeContainerStock}>
+                      <KeyboardArrowDownIcon className={styles.arrowIconDown} />
+                      <Typography className={styles.changeStock}>
+                        {change}
+                      </Typography>
+                    </div>
+                    <Typography className={styles.changeText}>
+                      Change 24hrs
+                    </Typography>
+                  </>
+                ) : category === "Crypto" ? (
+                  <>
+                    <div className={styles.changeContainerCrypto}>
+                      <KeyboardArrowUpIcon className={styles.arrowIconUp} />
+                      <Typography className={styles.changeCrypto}>
+                        {change}
+                      </Typography>
+                    </div>
+                    <Typography className={styles.changeText}>
+                      Change 24hrs
+                    </Typography>
+                  </>
+                ) : (
+                  <div className={styles.changeContainerNFT}>
+                    <Typography className={styles.ethereumText}>
+                      Ethereum
+                    </Typography>
+                    <Typography className={styles.ethereumText}>
+                      Crypto Chain
+                    </Typography>
+                  </div>
+                )}
+
                 <Button variant="contained" className={styles.button}>
                   View report
                 </Button>
